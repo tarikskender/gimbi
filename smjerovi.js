29b0f45a8d05b6bfcc28ed72db2e717e5a2914a8
kumicija <119958761+kumicija@users.noreply.github.com>
2023-03-11 13:45:22 +0100

    Update smjerovi.js

--- smjerovi.js
+++ smjerovi.js
@@ -1,25 +1,29 @@
-window.onload = (hideAll(0));
+function show(i){
 
-/*function opciFunction(){
+    var id = "desc-" + i.toString(), idn = "#" + id, fI = true, ids = "select-"+i.toString();
 
-}
-
-function racFunction(){
-
-}
-
-function nFunc(){
-
-}*/
-
-function dsdFunc(){
-    document.getElementById("i4").style.visibility = "visible";
-    hideAll(4);
-}
-
-function hideAll(x){
-    for(var i = 0; i<=4; i++){
-        if(i!=x) document.getElementById("i"+x.toString()).style.visibility = "hidden";
+    var x = document.getElementById(id);
+    var b = document.getElementById(ids);
+    if (x.style.display === "block") {
+        x.style.animation  = "fadeOut .5s forwards";
+        if(fI == true) fI = false;
+        b.innerHTML = "Pokaži više";
+    } else {
+        x.style.animation  = "fadeIn .5s forwards";
+        x.style.display = "block";
+        b.innerHTML = "Pokaži manje";
     }
-    
+
+    const animated = document.querySelector(idn);
+    animated.addEventListener("animationend", () => {
+        if(x.style.display === "block" && fI == false){
+            x.style.display = "none";
+            b.innerHTML = "Pokaži više";
+        }else{
+            x.style.display = "block"; 
+            b.innerHTML = "Pokaži manje";
+        } 
+    });
 }
+
+
